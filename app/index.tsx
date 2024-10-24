c9435e81f12d8ea343ca57fa1d6b3644fb4065f4
LucilaAllende <abigail_lucii@hotmail.com>
2024-10-24 16:09:09 -0300

    Refactor gitignore and Colors.ts
    - Remove unused files and code
    - Update gitignore to ignore expo-generated files
    - Add new color constants to Colors.ts

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Page: React.FC = () => {
+  return (
+    <View>
+      <Text>Welcome to the Page Component!</Text>
+    </View>
+  );
+};
+
+export default Page;
